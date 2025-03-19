f4d43f94a99f6eb209954f1fba2e0d7b0cbff421
MarItzep <3077567260805@ingenieria.usac.edu.gt>
2025-03-19 13:22:07 -0600

    clase 8

---
+++ clases/clase 8/AVL/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+class Repuestos
+{
+    public int ID;
+    public string Repuesto;
+    public string Detalles;
+    public double Costo;
+
+    public Repuestos(int id, string repuesto, string detalle, double costo)
+    {
+        ID = id;
+        Repuesto = repuesto;
+        Detalles = detalle;
+        Costo = costo;
+
+    }
+}
+
+class AvlNode
+{
+
+    public Repuestos item;
+    public AvlNode? left;
+    public AvlNode? right;
+    public int height;
+
+    public AvlNode(Repuestos Item)
+    {
+        item = Item;
+        left = null;
+        right = null;
+        height = 0;
+    }
+
+
+}
+
+
+class AvlTree
+{
+    private AvlNode root;
+    string connections = "";
+    string nodes = "";
+
+    public void insert(Repuestos item)
+    {
+        root = insertRecursive(item, root);
+    }
+
+    public int GetHeight(AvlNode node)
+    {
+        return node == null ? -1 : node.height;
+    }
+
+    public int getMaxHeight(int leftNode, int rightNode)
+    {
+        return leftNode > rightNode ? leftNode : rightNode;
+    }
+
+   
+
+    public AvlNode insertRecursive(Repuestos item, AvlNode node)
+    {
+        if(node == null)
+        {
+            node = new AvlNode(item);
+        }
+        else if(item.ID < node.item.ID)
+        {
+            node.left = insertRecursive(item, node.left);
+
+            if(GetHeight(node.left) - GetHeight(node.right) == 2)
+            {
+                //derecho
+                if(item.ID < node.left.item.ID)
+                {
+                    node = rotateright(node);
+
+                } else 
+                {
+                    node = doubleRight(node);
+                }
+
+            }
+
+        } 
+        else if(item.ID > node.item.ID)
+        {
+            node.right = insertRecursive(item, node.right);
+            if(GetHeight(node.right) - GetHeight(node.left) == 2) 
+            {
+                //izquierdo
+                if(item.ID > node.right.item.ID)
+                {
+                    node = rotateleft(node);
+                }
+                else 
+                {
+                    node = doubleLeft(node);
+                }
+            } 
+
+        }
+        else 
+        {
+            Console.WriteLine("Elemento ya existe en el arbol");
+        }
+
+
+        node.height = getMaxHeight(GetHeight(node.left), GetHeight(node.right)) + 1;
+        return node;
+
+    }
+
+
+    public AvlNode rotateleft(AvlNode node1)
+    {
+        AvlNode node2 = node1.right;
+        node1.right = node2.left;
+        node2.left = node1;
+        node1.height = getMaxHeight(GetHeight(node1.left), this.GetHeight(node1.right)) + 1;
+        node2.height = getMaxHeight(GetHeight(node2.right), node1.height) + 1;
+        return node2;
+    }
+
+    public AvlNode rotateright(AvlNode node2)
+    {
+        AvlNode node1 = node2.left;
+        node2.left = node1.right;
+        node1.right = node2;
+        node2.height = getMaxHeight(GetHeight(node2.left), GetHeight(node2.right)) + 1;
+        node1.height = getMaxHeight(GetHeight(node1.left), node2.height) + 1;
+        return node1;
+    }
+
+    public AvlNode doubleLeft(AvlNode node)
+    {
+        node.left = rotateright(node.left);
+        return rotateleft(node);
+    }
+
+    public AvlNode doubleRight(AvlNode node)
+    {
+        node.right = rotateleft(node.right);
+        return rotateright(node);
+    }
+
+
+    public void treeGraph()
+    {
+        nodes = "";
+        connections = "";
+        treeGraphRecursive(root);
+
+        Console.WriteLine("digraph BST {");
+        Console.WriteLine("node [shape=rectangle];");
+        Console.WriteLine(nodes);
+        Console.WriteLine(connections);
+        Console.WriteLine("}");
+
+    }
+
+    public void treeGraphRecursive(AvlNode current)
+    {
+        if(current.left != null)
+        {
+            treeGraphRecursive(current.left);
+            connections += "S"+ Convert.ToString(current.item.ID) + "->" + "S" + Convert.ToString(current.left.item.ID) + "\n";
+
+        }
+        nodes += "S"+Convert.ToString(current.item.ID)+"[label="+Convert.ToString(current.item.ID)+"];";
+        if(current.right != null)
+        {
+            treeGraphRecursive(current.right);
+            connections += "S"+ Convert.ToString(current.item.ID) + "->" + "S" + Convert.ToString(current.right.item.ID) + "\n";
+        }
+
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Repuestos repuesto1 = new Repuestos(1, "motor", "nuevo", 50.2);
+        Repuestos repuesto2 = new Repuestos(2, "motor", "nuevo", 50.2);
+        Repuestos repuesto3 = new Repuestos(3, "motor", "nuevo", 50.2);
+        Repuestos repuesto4 = new Repuestos(4, "motor", "nuevo", 50.2);
+        Repuestos repuesto5 = new Repuestos(5, "motor", "nuevo", 50.2);
+        Repuestos repuesto6 = new Repuestos(6, "motor", "nuevo", 50.2);
+        Repuestos repuesto14 = new Repuestos(14, "motor", "nuevo", 50.2);
+        Repuestos repuesto15 = new Repuestos(15, "motor", "nuevo", 50.2);
+        Repuestos repuesto16 = new Repuestos(16, "motor", "nuevo", 50.2);
+        AvlTree arbol = new AvlTree();
+        arbol.insert(repuesto1);
+        arbol.insert(repuesto2);
+        arbol.insert(repuesto3);
+        arbol.insert(repuesto4);
+        arbol.insert(repuesto5);
+        arbol.insert(repuesto6);
+        arbol.insert(repuesto14);
+        arbol.insert(repuesto15);
+        arbol.insert(repuesto16);
+
+        //arbol.treeGraphRecursive();
+
+
+        arbol.treeGraph();
+    }
+}
---
+++ clases/clase 8/B5/Program.cs
@@ -0,0 +1,549 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Clase que representa un elemento con Id y Total
+public class Elemento
+{
+    public int Id { get; set; }
+    public double Total { get; set; }
+
+    public Elemento(int id, double total)
+    {
+        Id = id;
+        Total = total;
+    }
+}
+
+// Clase que representa un nodo en el árbol B
+public class NodoArbolB
+{
+    private const int ORDEN = 5;
+    private const int MAX_CLAVES = ORDEN - 1;
+    private const int MIN_CLAVES = (ORDEN / 2) - 1;
+    
+    public List<Elemento> Claves { get; set; }
+    public List<NodoArbolB> Hijos { get; set; }
+    public bool EsHoja { get; set; }
+
+    public NodoArbolB()
+    {
+        Claves = new List<Elemento>(MAX_CLAVES);
+        Hijos = new List<NodoArbolB>(ORDEN);
+        EsHoja = true;
+    }
+
+    // Verifica si el nodo está lleno
+    public bool EstaLleno()
+    {
+        return Claves.Count >= MAX_CLAVES;
+    }
+
+    // Verifica si el nodo tiene el mínimo de claves requerido
+    public bool TieneMinimoClaves()
+    {
+        return Claves.Count >= MIN_CLAVES;
+    }
+}
+
+public class ArbolB
+{
+    private NodoArbolB raiz;
+    private const int ORDEN = 5;
+    private const int MAX_CLAVES = ORDEN - 1;
+    private const int MIN_CLAVES = (ORDEN / 2) - 1;
+
+    public ArbolB()
+    {
+        raiz = new NodoArbolB();
+    }
+
+    // Método para insertar un nuevo elemento
+    public void Insertar(int id, double total)
+    {
+        Elemento nuevoElemento = new Elemento(id, total);
+        
+        // Si la raíz está llena, se crea una nueva raíz
+        if (raiz.EstaLleno())
+        {
+            NodoArbolB nuevaRaiz = new NodoArbolB();
+            nuevaRaiz.EsHoja = false;
+            nuevaRaiz.Hijos.Add(raiz);
+            DividirHijo(nuevaRaiz, 0);
+            raiz = nuevaRaiz;
+        }
+        
+        InsertarNoLleno(raiz, nuevoElemento);
+    }
+
+    // Divide un hijo cuando está lleno durante la inserción
+    private void DividirHijo(NodoArbolB padre, int indiceHijo)
+    {
+        NodoArbolB hijoCompleto = padre.Hijos[indiceHijo];
+        NodoArbolB nuevoHijo = new NodoArbolB();
+        nuevoHijo.EsHoja = hijoCompleto.EsHoja;
+
+        // Elemento del medio que se promoverá al padre
+        Elemento elementoMedio = hijoCompleto.Claves[MIN_CLAVES];
+
+        // Mover la mitad de las claves al nuevo hijo
+        for (int i = MIN_CLAVES + 1; i < MAX_CLAVES; i++)
+        {
+            nuevoHijo.Claves.Add(hijoCompleto.Claves[i]);
+        }
+
+        // Si no es hoja, mover también los hijos correspondientes
+        if (!hijoCompleto.EsHoja)
+        {
+            for (int i = (ORDEN / 2); i < ORDEN; i++)
+            {
+                nuevoHijo.Hijos.Add(hijoCompleto.Hijos[i]);
+            }
+            hijoCompleto.Hijos.RemoveRange((ORDEN / 2), hijoCompleto.Hijos.Count - (ORDEN / 2));
+        }
+
+        // Eliminar las claves movidas del hijo original
+        hijoCompleto.Claves.RemoveRange(MIN_CLAVES, hijoCompleto.Claves.Count - MIN_CLAVES);
+
+        // Insertar el nuevo hijo en el padre
+        padre.Hijos.Insert(indiceHijo + 1, nuevoHijo);
+
+        // Insertar la clave media en el padre
+        int j = 0;
+        while (j < padre.Claves.Count && padre.Claves[j].Id < elementoMedio.Id)
+        {
+            j++;
+        }
+        padre.Claves.Insert(j, elementoMedio);
+    }
+
+    // Inserta un elemento en un nodo que no está lleno
+    private void InsertarNoLleno(NodoArbolB nodo, Elemento elemento)
+    {
+        int i = nodo.Claves.Count - 1;
+
+        // Si es hoja, simplemente inserta el elemento en orden
+        if (nodo.EsHoja)
+        {
+            // Buscar la posición correcta para insertar
+            while (i >= 0 && elemento.Id < nodo.Claves[i].Id)
+            {
+                i--;
+            }
+            nodo.Claves.Insert(i + 1, elemento);
+        }
+        else
+        {
+            // Encuentra el hijo donde debe estar el elemento
+            while (i >= 0 && elemento.Id < nodo.Claves[i].Id)
+            {
+                i--;
+            }
+            i++;
+
+            // Si el hijo está lleno, divídelo primero
+            if (nodo.Hijos[i].EstaLleno())
+            {
+                DividirHijo(nodo, i);
+                if (elemento.Id > nodo.Claves[i].Id)
+                {
+                    i++;
+                }
+            }
+            InsertarNoLleno(nodo.Hijos[i], elemento);
+        }
+    }
+
+    // Busca un elemento por su Id
+    public Elemento Buscar(int id)
+    {
+        return BuscarRecursivo(raiz, id);
+    }
+
+    private Elemento BuscarRecursivo(NodoArbolB nodo, int id)
+    {
+        int i = 0;
+        // Buscar la primera clave mayor o igual que id
+        while (i < nodo.Claves.Count && id > nodo.Claves[i].Id)
+        {
+            i++;
+        }
+
+        // Si encontramos el id, devolvemos el elemento
+        if (i < nodo.Claves.Count && id == nodo.Claves[i].Id)
+        {
+            return nodo.Claves[i];
+        }
+
+        // Si es una hoja y no encontramos el id, no existe
+        if (nodo.EsHoja)
+        {
+            return null;
+        }
+
+        // Si no es hoja, buscamos en el hijo correspondiente
+        return BuscarRecursivo(nodo.Hijos[i], id);
+    }
+
+    // Método para eliminar un elemento por su Id
+    public void Eliminar(int id)
+    {
+        EliminarRecursivo(raiz, id);
+        
+        // Si la raíz quedó vacía pero tiene hijos, el primer hijo se convierte en la nueva raíz
+        if (raiz.Claves.Count == 0 && !raiz.EsHoja)
+        {
+            NodoArbolB antiguaRaiz = raiz;
+            raiz = raiz.Hijos[0];
+        }
+    }
+
+    private void EliminarRecursivo(NodoArbolB nodo, int id)
+    {
+        int indice = EncontrarIndice(nodo, id);
+
+        // Caso 1: La clave está en este nodo
+        if (indice < nodo.Claves.Count && nodo.Claves[indice].Id == id)
+        {
+            // Si es hoja, simplemente eliminamos
+            if (nodo.EsHoja)
+            {
+                nodo.Claves.RemoveAt(indice);
+            }
+            else
+            {
+                // Si no es hoja, usamos estrategias más complejas
+                EliminarDeNodoInterno(nodo, indice);
+            }
+        }
+        else
+        {
+            // Caso 2: La clave no está en este nodo
+            if (nodo.EsHoja)
+            {
+                Console.WriteLine($"El elemento con Id {id} no existe en el árbol");
+                return;
+            }
+
+            // Determinar si el último hijo fue visitado
+            bool ultimoHijo = (indice == nodo.Claves.Count);
+
+            // Si el hijo tiene el mínimo de claves, rellenarlo
+            if (!nodo.Hijos[indice].TieneMinimoClaves())
+            {
+                RellenarHijo(nodo, indice);
+            }
+
+            // Si el último hijo se fusionó, recurrimos al hijo anterior
+            if (ultimoHijo && indice > nodo.Hijos.Count - 1)
+            {
+                EliminarRecursivo(nodo.Hijos[indice - 1], id);
+            }
+            else
+            {
+                EliminarRecursivo(nodo.Hijos[indice], id);
+            }
+        }
+    }
+
+    // Encuentra el índice de la primera clave mayor o igual a id
+    private int EncontrarIndice(NodoArbolB nodo, int id)
+    {
+        int indice = 0;
+        while (indice < nodo.Claves.Count && nodo.Claves[indice].Id < id)
+        {
+            indice++;
+        }
+        return indice;
+    }
+
+    // Elimina un elemento de un nodo interno
+    private void EliminarDeNodoInterno(NodoArbolB nodo, int indice)
+    {
+        Elemento clave = nodo.Claves[indice];
+
+        // Caso 2a: Si el hijo anterior tiene más del mínimo de claves
+        if (nodo.Hijos[indice].Claves.Count > MIN_CLAVES)
+        {
+            // Reemplazar clave con el predecesor
+            Elemento predecesor = ObtenerPredecesor(nodo, indice);
+            nodo.Claves[indice] = predecesor;
+            EliminarRecursivo(nodo.Hijos[indice], predecesor.Id);
+        }
+        // Caso 2b: Si el hijo siguiente tiene más del mínimo de claves
+        else if (nodo.Hijos[indice + 1].Claves.Count > MIN_CLAVES)
+        {
+            // Reemplazar clave con el sucesor
+            Elemento sucesor = ObtenerSucesor(nodo, indice);
+            nodo.Claves[indice] = sucesor;
+            EliminarRecursivo(nodo.Hijos[indice + 1], sucesor.Id);
+        }
+        // Caso 2c: Si ambos hijos tienen el mínimo de claves
+        else
+        {
+            // Fusionar el hijo actual con el siguiente
+            FusionarNodos(nodo, indice);
+            EliminarRecursivo(nodo.Hijos[indice], clave.Id);
+        }
+    }
+
+    // Obtiene el predecesor de una clave (la clave más grande en el subárbol izquierdo)
+    private Elemento ObtenerPredecesor(NodoArbolB nodo, int indice)
+    {
+        NodoArbolB actual = nodo.Hijos[indice];
+        while (!actual.EsHoja)
+        {
+            actual = actual.Hijos[actual.Claves.Count];
+        }
+        return actual.Claves[actual.Claves.Count - 1];
+    }
+
+    // Obtiene el sucesor de una clave (la clave más pequeña en el subárbol derecho)
+    private Elemento ObtenerSucesor(NodoArbolB nodo, int indice)
+    {
+        NodoArbolB actual = nodo.Hijos[indice + 1];
+        while (!actual.EsHoja)
+        {
+            actual = actual.Hijos[0];
+        }
+        return actual.Claves[0];
+    }
+
+    // Rellena un hijo que tiene menos del mínimo de claves
+    private void RellenarHijo(NodoArbolB nodo, int indice)
+    {
+        // Si el hermano izquierdo existe y tiene más del mínimo de claves
+        if (indice > 0 && nodo.Hijos[indice - 1].Claves.Count > MIN_CLAVES)
+        {
+            TomaPrestadoDelAnterior(nodo, indice);
+        }
+        // Si el hermano derecho existe y tiene más del mínimo de claves
+        else if (indice < nodo.Claves.Count && nodo.Hijos[indice + 1].Claves.Count > MIN_CLAVES)
+        {
+            TomaPrestadoDelSiguiente(nodo, indice);
+        }
+        // Si no se puede tomar prestado, fusionar con un hermano
+        else
+        {
+            if (indice < nodo.Claves.Count)
+            {
+                FusionarNodos(nodo, indice);
+            }
+            else
+            {
+                FusionarNodos(nodo, indice - 1);
+            }
+        }
+    }
+
+    // Toma prestado una clave del hermano anterior
+    private void TomaPrestadoDelAnterior(NodoArbolB nodo, int indice)
+    {
+        NodoArbolB hijo = nodo.Hijos[indice];
+        NodoArbolB hermano = nodo.Hijos[indice - 1];
+
+        // Desplazar todas las claves e hijos para hacer espacio para la nueva clave
+        hijo.Claves.Insert(0, nodo.Claves[indice - 1]);
+
+        // Si no es hoja, mover también el hijo correspondiente
+        if (!hijo.EsHoja)
+        {
+            hijo.Hijos.Insert(0, hermano.Hijos[hermano.Claves.Count]);
+            hermano.Hijos.RemoveAt(hermano.Claves.Count);
+        }
+
+        // Actualizar la clave del padre
+        nodo.Claves[indice - 1] = hermano.Claves[hermano.Claves.Count - 1];
+        hermano.Claves.RemoveAt(hermano.Claves.Count - 1);
+    }
+
+    // Toma prestado una clave del hermano siguiente
+    private void TomaPrestadoDelSiguiente(NodoArbolB nodo, int indice)
+    {
+        NodoArbolB hijo = nodo.Hijos[indice];
+        NodoArbolB hermano = nodo.Hijos[indice + 1];
+
+        // Añadir la clave del padre al hijo
+        hijo.Claves.Add(nodo.Claves[indice]);
+
+        // Si no es hoja, mover también el hijo correspondiente
+        if (!hijo.EsHoja)
+        {
+            hijo.Hijos.Add(hermano.Hijos[0]);
+            hermano.Hijos.RemoveAt(0);
+        }
+
+        // Actualizar la clave del padre
+        nodo.Claves[indice] = hermano.Claves[0];
+        hermano.Claves.RemoveAt(0);
+    }
+
+    // Fusiona dos nodos hijo
+    private void FusionarNodos(NodoArbolB nodo, int indice)
+    {
+        NodoArbolB hijo = nodo.Hijos[indice];
+        NodoArbolB hermano = nodo.Hijos[indice + 1];
+
+        // Añadir la clave del padre al hijo
+        hijo.Claves.Add(nodo.Claves[indice]);
+
+        // Añadir todas las claves del hermano al hijo
+        for (int i = 0; i < hermano.Claves.Count; i++)
+        {
+            hijo.Claves.Add(hermano.Claves[i]);
+        }
+
+        // Si no es hoja, mover también los hijos
+        if (!hijo.EsHoja)
+        {
+            for (int i = 0; i < hermano.Hijos.Count; i++)
+            {
+                hijo.Hijos.Add(hermano.Hijos[i]);
+            }
+        }
+
+        // Remover la clave y el hijo del nodo padre
+        nodo.Claves.RemoveAt(indice);
+        nodo.Hijos.RemoveAt(indice + 1);
+    }
+
+    // Recorrido InOrden del árbol
+    public List<Elemento> RecorridoInOrden()
+    {
+        List<Elemento> resultado = new List<Elemento>();
+        RecorridoInOrdenRecursivo(raiz, resultado);
+        return resultado;
+    }
+
+    private void RecorridoInOrdenRecursivo(NodoArbolB nodo, List<Elemento> resultado)
+    {
+        if (nodo == null)
+            return;
+
+        int i;
+        for (i = 0; i < nodo.Claves.Count; i++)
+        {
+            // Recorrer el hijo izquierdo
+            if (!nodo.EsHoja)
+                RecorridoInOrdenRecursivo(nodo.Hijos[i], resultado);
+
+            // Agregar la clave actual
+            resultado.Add(nodo.Claves[i]);
+        }
+
+        // Recorrer el último hijo
+        if (!nodo.EsHoja)
+            RecorridoInOrdenRecursivo(nodo.Hijos[i], resultado);
+    }
+
+    // Método para generar el archivo .dot para Graphviz
+    public string GraficarGraphviz()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("digraph BTree {");
+        sb.AppendLine("    node [shape=record];");
+        
+        int contadorNodos = 0;
+        GraficarGraphvizRecursivo(raiz, sb, ref contadorNodos);
+        
+        sb.AppendLine("}");
+        Console.WriteLine(sb.ToString());
+        return sb.ToString();
+    }
+
+    private void GraficarGraphvizRecursivo(NodoArbolB nodo, StringBuilder sb, ref int contadorNodos)
+    {
+        if (nodo == null)
+            return;
+
+        int nodoActual = contadorNodos++;
+        
+        // Construir la etiqueta del nodo
+        StringBuilder etiquetaNodo = new StringBuilder();
+        etiquetaNodo.Append($"node{nodoActual} [label=\"");
+        
+        for (int i = 0; i < nodo.Claves.Count; i++)
+        {
+            if (i > 0)
+                etiquetaNodo.Append("|");
+            etiquetaNodo.Append($"<f{i}> |Id: {nodo.Claves[i].Id}, Total: {nodo.Claves[i].Total}|");
+        }
+        
+        // Añadir un puerto más para el último hijo
+        if (nodo.Claves.Count > 0)
+            etiquetaNodo.Append($"<f{nodo.Claves.Count}>");
+        
+        etiquetaNodo.Append("\"];");
+        sb.AppendLine(etiquetaNodo.ToString());
+        
+        // Graficar los hijos y sus conexiones
+        if (!nodo.EsHoja)
+        {
+            for (int i = 0; i <= nodo.Claves.Count; i++)
+            {
+                int hijoPosicion = contadorNodos;
+                GraficarGraphvizRecursivo(nodo.Hijos[i], sb, ref contadorNodos);
+                sb.AppendLine($"    node{nodoActual}:f{i} -> node{hijoPosicion};");
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creamos una instancia del árbol B
+        ArbolB arbol = new ArbolB();
+        
+        // Insertamos elementos
+        arbol.Insertar(10, 100.50);
+        arbol.Insertar(5, 200.75);
+        arbol.Insertar(15, 300.25);
+        arbol.Insertar(7, 400.00);
+        arbol.Insertar(12, 500.30);
+        arbol.Insertar(2, 300.50);
+        /*
+        arbol.Insertar(17, 400.75);
+        arbol.Insertar(22, 500.25);
+        arbol.Insertar(30, 600.00);
+        arbol.Insertar(25, 700.30);
+        arbol.Insertar(3, 800.50);
+        arbol.Insertar(18, 900.75);
+        
+        */
+        /*
+        arbol.Insertar(33, 800.50);
+        arbol.Insertar(48, 900.75); 
+        arbol.Insertar(49, 900.75); 
+        arbol.Insertar(58, 900.75); 
+        arbol.Insertar(68, 900.75); 
+*/
+        /*
+        arbol.Insertar(2, 300.50);
+        arbol.Insertar(17, 400.75);
+        arbol.Insertar(22, 500.25);
+        arbol.Insertar(30, 600.00);
+        arbol.Insertar(25, 700.30);
+        arbol.Insertar(3, 800.50);
+        arbol.Insertar(18, 900.75);
+        arbol.Insertar(33, 800.50);
+        arbol.Insertar(48, 900.75);        
+       */
+        Console.WriteLine("Árbol B de orden 5 creado");
+        
+        // Generar representación gráfica
+        arbol.GraficarGraphviz();
+        
+        // Mostrar elementos en orden
+        Console.WriteLine("\nRecorrido InOrden del árbol:");
+        arbol.RecorridoInOrden().ForEach(elem => 
+            Console.WriteLine($"Id: {elem.Id}, Total: {elem.Total}"));
+            
+
+        Console.WriteLine("\nÁrbol después de eliminar:");
+        int id = 1;
+        arbol.Eliminar(id);
+        arbol.GraficarGraphviz();
+    }
+}
